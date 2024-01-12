73125cf0008f51af81285d70ff2bd2a63ae90e76
Aryan12Nt <aryanu.nt@gmail.com>
2024-01-12 10:57:30 +0530

    created model

---
+++ lib/model.dart
@@ -0,0 +1,15 @@
+class ResumeItem {
+  String? title;
+  String? email;
+  String? phoneNumber;
+  String? address;
+  String? description;
+
+  ResumeItem({
+    this.title,
+    this.email,
+    this.phoneNumber,
+    this.address,
+    this.description,
+  });
+}
